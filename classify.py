db39f4d10051352888962bed0e1f881b41496a42
Helen <heleny@princeton.edu>
2015-02-17 15:50:46 -0500

    print out selected features

--- classify.py
+++ classify.py
@@ -16,6 +16,10 @@ from sklearn.ensemble import AdaBoostClassifier
 num_train = 45000
 num_test = 5000
 select_features = True
+percentile = 10
+freq_cutoff = 2.5
+class_prior = [.2, .8]
+rounds = 50
 
 # reading a bag of words file back into python. The number and order
 # of emails should be the same as in the *samples_class* file.
@@ -23,6 +27,20 @@ def read_bagofwords_dat(myfile, numofemails):
     bagofwords = numpy.fromfile(myfile, dtype=numpy.uint8, count=-1, sep="")
     bagofwords=numpy.reshape(bagofwords,(numofemails,-1))
     return bagofwords
+
+def print_features(mask):
+    vocab = []
+    with open("trec07p_data/Test/train_emails_vocab_200.txt") as f:
+        vocab = f.readlines()
+
+    vocab = [x.strip('\n') for x in vocab]
+
+    print "Retained vocabulary:"
+    for i in range(0, num_test):
+        if mask[i]:
+            print vocab[i], 
+    print "\n-----------------------"
+
 
 def main():
     # Bag of words for training data
@@ -49,21 +67,22 @@ def main():
 
 
     if select_features:
-        selector = feature_selection.SelectPercentile(feature_selection.f_classif, percentile=10 )
+        selector = feature_selection.SelectPercentile(feature_selection.f_classif, percentile=percentile)
         train = selector.fit_transform(train, train_target)
         test = selector.transform(test)
-        print "Finished doing feature selection"
+        
+        mask = selector.get_support()
+        #print_features(mask)
+        print ("Finished doing %d percentile feature selection" % (percentile))
 
-    cutoff = 2.5
-    class_prior = [.2, .8]
     classifiers = [
-        (svm.LinearSVC(), "SVML"),
-        (tree.DecisionTreeClassifier(), "Decision Tree"), 
-        (GaussianNB(), "Gaussian"), 
-        (MultinomialNB(1.0, False, class_prior), "Multinomial"), 
-        (BernoulliNB(1.0, cutoff, False, class_prior), "Bernoulli"),
-        (AdaBoostClassifier(base_estimator = tree.DecisionTreeClassifier(max_depth=3), n_estimators = 10), 
-        "Adaboost with max-depth 3 decision tree")
+#        (svm.LinearSVC(), "SVML"),
+#        (tree.DecisionTreeClassifier(), "Decision Tree"), 
+#        (GaussianNB(), "Gaussian"), 
+#        (MultinomialNB(1.0, False, class_prior), "Multinomial"), 
+#        (BernoulliNB(1.0, freq_cutoff, False, class_prior), "Bernoulli"), 
+#        (AdaBoostClassifier(base_estimator = tree.DecisionTreeClassifier(max_depth=3), n_estimators = rounds), 
+#        "Adaboost with %d rounds and max-depth 3 decision tree" % (rounds))
         ]
     for (classifier, name) in classifiers: 
         model = classifier.fit(train, train_target)
@@ -81,18 +100,6 @@ def main():
 
         print("%s: FP %d, FN %d, TP %d " % (name, FP, FN, TP))
 '''
-    vocab = []
-    with open("trec07p_data/Test/train_emails_vocab_200.txt") as f:
-        vocab = f.readlines()
-
-    vocab = [x.strip('\n') for x in vocab]
-
-    for i in range(0, num_test):
-        if y_pred[i] != test_target[i]:
-            print y_pred[i], 
-            for j in range(0, len(test[i])):
-                if test[i][j] > cutoff: print vocab[j],
-            print "\n -----------------------"
 '''
 
 main()
